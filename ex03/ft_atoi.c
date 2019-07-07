15e68618fc9331341267339e8c7139950443d721
tin <tin@e2r7p21.42.fr>
2019-07-07 20:36:38 +0200

    norminette ok - atois shortened

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,17 +6,17 @@
 /*   By: tin <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/07 15:26:39 by tin               #+#    #+#             */
-/*   Updated: 2019/07/07 15:26:42 by tin              ###   ########.fr       */
+/*   Updated: 2019/07/07 20:35:22 by tin              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-	int i;
-	long output;
-	int sign;
+	int		i;
+	long	output;
+	int		sign;
 
 	i = 0;
 	sign = 1;
@@ -25,19 +25,14 @@ int ft_atoi(char *str)
 		i++;
 	while (str[i] == '-' || str[i] == '+')
 	{
-		if(str[i]  == '-')
+		if (str[i] == '-')
 			sign = -1 * sign;
 		i++;
 	}
-	if(str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		while(str[i] >= '0' && str[i] <= '9')
-		{
-			output = output * 10 + str[i] - '0';
-			i++;
-		}
-	return ((int)output * sign);
+		output = output * 10 + str[i] - '0';
+		i++;
 	}
-	else
-		return (0);
+	return ((int)output * sign);
 }
--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -6,36 +6,36 @@
 /*   By: tin <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/07 15:26:53 by tin               #+#    #+#             */
-/*   Updated: 2019/07/07 15:26:55 by tin              ###   ########.fr       */
+/*   Updated: 2019/07/07 20:23:59 by tin              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-long base_settings(char *base)
+long	base_settings(char *base)
 {
-	long i;
-	int output;
+	long	i;
+	int		output;
 
 	i = 0;
 	output = 0;
-	while(base[i] != '\0')
+	while (base[i] != '\0')
 	{
 		if ((base[i] >= 'A' && base[i] <= 'Z') ||
 			(base[i] >= 'a' && base[i] <= 'z') ||
 				(base[i] >= '0' && base[i] <= '9'))
-					output++;
+			output++;
 		i++;
-		if(output != i)
+		if (output != i)
 			output = 0;
 	}
-	if(output == 1 || output == 0)
-		return(0);
-	return (output );
+	if (output == 1 || output == 0)
+		return (0);
+	return (output);
 }
 
-
-long max_multipl_base(char *base){
+long	max_multipl_base(char *base)
+{
 	long max_int;
 	long max_multipl;
 	long born_base;
@@ -43,49 +43,49 @@ long max_multipl_base(char *base){
 	max_int = 2147483648;
 	born_base = base_settings(base);
 	max_multipl = born_base;
-	while(max_int)
+	while (max_int)
 	{
-			max_int = max_int / born_base;
-			max_multipl = max_multipl * born_base;
+		max_int = max_int / born_base;
+		max_multipl = max_multipl * born_base;
 	}
 	return (max_multipl);
 }
 
-void print_number(long nb, char *base, long max_multipl)
+void	print_number(long nb, char *base, long max_multipl)
 {
-	char c;
-	long born_base;
-	int firstzeros;
+	char	c;
+	long	born_base;
+	int		firstzeros;
+
 	firstzeros = 1;
 	born_base = base_settings(base);
-
-	while(max_multipl)
+	while (max_multipl)
 	{
 		c = base[nb / max_multipl];
 		nb = nb - (nb / max_multipl) * max_multipl;
 		max_multipl = max_multipl / born_base;
-		if(firstzeros && (nb / max_multipl))
+		if (firstzeros && (nb / max_multipl))
 		{
 			firstzeros = 0;
 			continue;
 		}
-		if(!firstzeros)
+		if (!firstzeros)
 			write(1, &c, 1);
 	}
 }
 
-void ft_putnbr_base(int nbr, char *base)
+void	ft_putnbr_base(int nbr, char *base)
 {
 	long l_nbr;
 	char c;
 
-	l_nbr = (long) nbr;
+	l_nbr = (long)nbr;
 	c = '-';
-	if(l_nbr < 0)
+	if (l_nbr < 0)
 	{
 		write(1, &c, 1);
 		l_nbr = -1 * l_nbr;
 	}
-	if(base_settings(base))
+	if (base_settings(base))
 		print_number(l_nbr, base, max_multipl_base(base));
 }
